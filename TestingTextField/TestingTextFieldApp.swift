247255dc69bea214dafb657e5db4166b17cdf8d0
Azura Sakan Taufik <azstima@gmail.com>
2022-11-23 12:35:47 +0700

    Initial Commit

---
+++ TestingTextField/TestingTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingTextFieldApp.swift
+//  TestingTextField
+//
+//  Created by TI Digital on 23/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TestingTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
